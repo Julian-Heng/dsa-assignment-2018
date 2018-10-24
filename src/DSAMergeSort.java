07a5a6659551073414c0b2886fed8ebf25255b2f
Julian Heng <19473701@student.curtin.edu.au>
2018-10-25 01:03:05 +0800

    Move merge sort to a seperate file

---
+++ src/DSAMergeSort.java
@@ -0,0 +1,59 @@
+public class DSAMergeSort
+{
+    public static void mergeSort(int[] A)
+    {
+        recurseMergeSort(A, 0, A.length - 1);
+    }
+
+    public static void recurseMergeSort(int[] A, int leftIndex, int rightIndex)
+    {
+        int midIndex;
+        if (leftIndex < rightIndex)
+        {
+            midIndex = (leftIndex + rightIndex) / 2;
+            recurseMergeSort(A, leftIndex, midIndex);
+            recurseMergeSort(A, midIndex + 1, rightIndex);
+            merge(A, leftIndex, midIndex, rightIndex);
+        }
+    }
+
+    public static void merge(int[] A, int leftIndex, int midIndex, int rightIndex)
+    {
+        int[] tempArr = new int[rightIndex - leftIndex + 1];
+        int i = leftIndex;
+        int j = midIndex + 1;
+        int k = 0;
+
+        while ((i <= midIndex) && (j <= rightIndex))
+        {
+            if (A[i] <= A[j])
+            {
+                tempArr[k] = A[i];
+                i++;
+            }
+            else
+            {
+                tempArr[k] = A[j];
+                j++;
+            }
+            k++;
+        }
+
+        for (int ii = i; ii <= midIndex; ii++)
+        {
+            tempArr[k] = A[ii];
+            k++;
+        }
+
+        for (int jj = j; jj <= rightIndex; jj++)
+        {
+            tempArr[k] = A[jj];
+            k++;
+        }
+
+        for (int kk = leftIndex; kk <= rightIndex; kk++)
+        {
+            A[kk] = tempArr[kk - leftIndex];
+        }
+    }
+}
--- src/Driver.java
+++ src/Driver.java
@@ -1120,7 +1120,7 @@ public class Driver
             }
             else
             {
-                mergeSort(visitDivisionIndex);
+                DSAMergeSort.mergeSort(visitDivisionIndex);
             }
         }
 
@@ -1544,71 +1544,9 @@ public class Driver
         return isUnique;
     }
 
-    public static void mergeSort(int[] A)
-    {
-        recurseMergeSort(A, 0, A.length - 1);
-    }
-
-    public static void recurseMergeSort(int[] A, int leftIndex, int rightIndex)
-    {
-        int midIndex;
-        if (leftIndex < rightIndex)
-        {
-            midIndex = (leftIndex + rightIndex) / 2;
-            recurseMergeSort(A, leftIndex, midIndex);
-            recurseMergeSort(A, midIndex + 1, rightIndex);
-            merge(A, leftIndex, midIndex, rightIndex);
-        }
-    }
-
-    public static void merge(int[] A, int leftIndex, int midIndex, int rightIndex)
-    {
-        int[] tempArr = new int[rightIndex - leftIndex + 1];
-        int i = leftIndex;
-        int j = midIndex + 1;
-        int k = 0;
-
-        while ((i <= midIndex) && (j <= rightIndex))
-        {
-            if (A[i] <= A[j])
-            {
-                tempArr[k] = A[i];
-                i++;
-            }
-            else
-            {
-                tempArr[k] = A[j];
-                j++;
-            }
-            k++;
-        }
-
-        for (int ii = i; ii <= midIndex; ii++)
-        {
-            tempArr[k] = A[ii];
-            k++;
-        }
-
-        for (int jj = j; jj <= rightIndex; jj++)
-        {
-            tempArr[k] = A[jj];
-            k++;
-        }
-
-        for (int kk = leftIndex; kk <= rightIndex; kk++)
-        {
-            A[kk] = tempArr[kk - leftIndex];
-        }
-    }
-
     public static boolean compareIntString(int num, String str)
     {
         return Integer.toString(num).equals(str);
-    }
-
-    public static void printSpinner(String[] pool, int step)
-    {
-        System.out.printf("%s\u0008", pool[step % pool.length]);
     }
 
     public static String toMiliseconds(long nano)

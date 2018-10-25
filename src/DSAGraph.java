0d5eb5b9f1978c132c3ed987caea4082e8984344
Julian Heng <19473701@student.curtin.edu.au>
2018-10-25 17:26:11 +0800

    Add commenting to DSAGraph

--- src/DSAGraph.java
+++ src/DSAGraph.java
@@ -1,8 +1,25 @@
 import java.util.*;
 import java.io.*;
 
+/**
+ *  Name:     DSAGraph
+ *  Source:   Practical 6
+ *
+ *  Modifications:
+ *     Implemented Dijkstra's algorithm and removed unneccessary
+ *     functions like the printing functions for adjacency list and
+ *     adjacency matrix
+ *
+ *  Author:   Julian Heng (19473701)
+ **/
+
 public class DSAGraph<E,F>
 {
+    /**
+     *  Name:     DSAGraphVertex
+     *  Purpose:  A graph vertex node in the graph
+     **/
+
     private class DSAGraphVertex<E,F>
     {
         private String label;
@@ -13,6 +30,7 @@ public class DSAGraph<E,F>
         private int distanceFromSource;
         private boolean visited;
 
+        // Alternate Constructor
         public DSAGraphVertex(String newLabel, E newValue)
         {
             label = newLabel;
@@ -24,6 +42,7 @@ public class DSAGraph<E,F>
             visited = false;
         }
 
+        // Getters
         public String getLabel() { return label; }
         public E getValue() { return value; }
 
@@ -38,6 +57,7 @@ public class DSAGraph<E,F>
             edgeValue = null;
             match = false;
 
+            // Loop until edge is found
             while (iter.hasNext() && ! match)
             {
                 inEdgeList = iter.next();
@@ -62,6 +82,7 @@ public class DSAGraph<E,F>
             iter = edgeList.iterator();
             match = false;
 
+            // Loop until edge is found
             while (iter.hasNext() && ! match)
             {
                 inEdgeList = iter.next();
@@ -86,6 +107,7 @@ public class DSAGraph<E,F>
             return edgeList;
         }
 
+        // Setters
         public void addEdge(
             DSAGraphVertex<E,F> newVertex,
             int edgeWeight,
@@ -96,6 +118,7 @@ public class DSAGraph<E,F>
 
             if (validateVertex(newVertex))
             {
+                // Create edge object
                 edgeLabel = label + " <-> " + newVertex.getLabel();
                 edge = new DSAGraphEdge<E,F>(edgeLabel, edgeWeight, edgeValue);
                 edge.setFrom(this);
@@ -161,6 +184,11 @@ public class DSAGraph<E,F>
         }
     }
 
+    /**
+     *  Name:     DSAGraphEdge
+     *  Purpose:  A graph edge node to hold edge weight and object
+     **/
+
     private class DSAGraphEdge<E,F>
     {
         String label;
@@ -169,6 +197,7 @@ public class DSAGraph<E,F>
         DSAGraphVertex<E,F> fromVertex, toVertex;
         boolean visited;
 
+        // Alternate Constructor
         public DSAGraphEdge(String inLabel, int inWeight, F inValue)
         {
             label = inLabel;
@@ -177,6 +206,7 @@ public class DSAGraph<E,F>
             visited = false;
         }
 
+        // Setters
         public void setVisited() { visited = true; }
         public void clearVisited() { visited = false; }
 
@@ -190,6 +220,7 @@ public class DSAGraph<E,F>
             toVertex = inTo;
         }
 
+        // Getters
         public String getLabel() { return label; }
         public int getEdgeWeight() { return weight; }
         public F getEdgeValue() { return edgeValue; }
@@ -203,12 +234,24 @@ public class DSAGraph<E,F>
     private int vertexCount;
     private int edgeCount;
 
+    // Default Constructor
     public DSAGraph()
     {
         vertices = new DSALinkedList<DSAGraphVertex<E,F>>();
         vertexCount = 0;
         edgeCount = 0;
     }
+
+    /**
+     *  Name:     addVertex
+     *  Purpose:  Add a vertex to the list of verticies
+     *  Imports:
+     *    - newLabel : A String for the label
+     *    - newValue : A Generic object for the value contained
+     *                 in the vertex
+     *  Exports:
+     *    - none
+     **/
 
     public void addVertex(String newLabel, E newValue)
     {
@@ -220,22 +263,29 @@ public class DSAGraph<E,F>
         }
     }
 
+    /**
+     *  Name:     addVertex
+     *  Purpose:  Add a vertex to the list of verticies
+     *  Imports:
+     *    - inVertex : A vertex object
+     *  Exports:
+     *    - none
+     **/
+
     public void addVertex(DSAGraphVertex<E,F> inVertex)
     {
         vertices.insertLast(inVertex);
         vertexCount++;
     }
 
-    public void addEdge(
-        DSAGraphVertex<E,F> vertex1,
-        DSAGraphVertex<E,F> vertex2,
-        int edgeWeight,
-        F edgeValue)
-    {
-        vertex1.addEdge(vertex2, edgeWeight, edgeValue);
-        vertex2.addEdge(vertex1, edgeWeight, edgeValue);
-        edgeCount++;
-    }
+    /**
+     *  Name:     addEdge
+     *  Purpose:  
+     *  Imports:
+     *    - 
+     *  Exports:
+     *    - 
+     **/
 
     public void addEdge(
         String label1,
@@ -251,22 +301,66 @@ public class DSAGraph<E,F>
         this.addEdge(vertex1, vertex2, edgeWeight, edgeValue);
     }
 
+    /**
+     *  Name:     addEdge
+     *  Purpose:  Add an edge in between 2 vertecies
+     *  Imports:
+     *    - vertex1    : Source vertex
+     *    - vertex2    : Target vertex to connect the edge to
+     *    - edgeWeight : The weight of the edge
+     *    - edgeValue  : A generic for the value contained in the edge
+     *  Exports:
+     *    - none
+     **/
+
+    public void addEdge(
+        DSAGraphVertex<E,F> vertex1,
+        DSAGraphVertex<E,F> vertex2,
+        int edgeWeight,
+        F edgeValue)
+    {
+        vertex1.addEdge(vertex2, edgeWeight, edgeValue);
+        vertex2.addEdge(vertex1, edgeWeight, edgeValue);
+        edgeCount++;
+    }
+
+    /**
+     *  Name:     getVertexValue
+     *  Purpose:  Get the value in a selected vertex
+     *  Imports:
+     *    - label : Search label
+     *  Exports:
+     *    - Generic object
+     **/
+
     public E getVertexValue(String label)
     {
         return this.getVertexValue(getVertex(label));
     }
+
+    /**
+     *  Name:     getVertexValue
+     *  Purpose:  Get the value in a selected vertex
+     *  Imports:
+     *    - vertex1 : The search vertex
+     *  Exports:
+     *    - Generic object
+     **/
 
     public E getVertexValue(DSAGraphVertex<E,F> vertex1)
     {
         return vertex1.getValue();
     }
 
-    public F getEdgeValue(
-        DSAGraphVertex<E,F> vertex1,
-        DSAGraphVertex<E,F> vertex2)
-    {
-        return vertex1.getEdgeValue(vertex2);
-    }
+    /**
+     *  Name:     getEdgeValue
+     *  Purpose:  Get the value in a selected edge
+     *  Imports:
+     *    - label1 : Search label 1
+     *    - label2 : Search label 2
+     *  Exports:
+     *    - Generic Object
+     **/
 
     public F getEdgeValue(String label1, String label2)
     {
@@ -281,12 +375,32 @@ public class DSAGraph<E,F>
         return this.getEdgeValue(vertex1, vertex2);
     }
 
-    public int getEdgeWeight(
+    /**
+     *  Name:     getEdgeValue
+     *  Purpose:  Get the value in a selected edge
+     *  Imports:
+     *    - vertex1 : The search vertex 1
+     *    - vertex2 : The serach vertex 2
+     *  Exports:
+     *    - Generic object
+     **/
+
+    public F getEdgeValue(
         DSAGraphVertex<E,F> vertex1,
         DSAGraphVertex<E,F> vertex2)
     {
-        return vertex1.getEdgeWeight(vertex2);
+        return vertex1.getEdgeValue(vertex2);
     }
+
+    /**
+     *  Name:     getEdgeWeight
+     *  Purpose:  Get the edge weight in between 2 vertex
+     *  Imports:
+     *    - label1 : The search label 1
+     *    - label2 : The search label 2
+     *  Exports:
+     *    - Integer
+     **/
 
     public int getEdgeWeight(String label1, String label2)
     {
@@ -301,15 +415,59 @@ public class DSAGraph<E,F>
         return this.getEdgeWeight(vertex1, vertex2);
     }
 
+    /**
+     *  Name:     getEdgeWeight
+     *  Purpose:  Get the edge weight in between 2 vertex
+     *  Imports:
+     *    - vertex1 : The search vertex 1
+     *    - vertex2 : The search vertex 2
+     *  Exports:
+     *    - Integer
+     **/
+
+    public int getEdgeWeight(
+        DSAGraphVertex<E,F> vertex1,
+        DSAGraphVertex<E,F> vertex2)
+    {
+        return vertex1.getEdgeWeight(vertex2);
+    }
+
+    /**
+     *  Name:     getVertexCount
+     *  Purpose:  Get the number of vertices
+     *  Imports:
+     *    - none
+     *  Exports:
+     *    - vertexCount : Integer
+     **/
+
     public int getVertexCount()
     {
         return vertexCount;
     }
 
+    /**
+     *  Name:     getEdgeCount
+     *  Purpose:  Get the number of edges
+     *  Imports:
+     *    - none
+     *  Exports:
+     *    - edgeCount : Integer
+     **/
+
     public int getEdgeCount()
     {
         return edgeCount;
     }
+
+    /**
+     *  Name:     getVertex
+     *  Purpose:  Get the vertex by label
+     *  Imports:
+     *    - searchLabel : String for label
+     *  Exports:
+     *    - findVertex : The vertex with the search label
+     **/
 
     public DSAGraphVertex<E,F> getVertex(String searchLabel)
     {
@@ -336,11 +494,30 @@ public class DSAGraph<E,F>
         return findVertex;
     }
 
+    /**
+     *  Name:     getAdjacent
+     *  Purpose:  Get the adjacent of a vertex
+     *  Imports:
+     *    - vertex : The vertex to get the adjacency list
+     *  Exports:
+     *    - Linked list of the adjacent vertices
+     **/
+
     public DSALinkedList<DSAGraphVertex<E,F>> getAdjacent(
         DSAGraphVertex<E,F> vertex)
     {
         return vertex.getAdjacent();
     }
+
+    /**
+     *  Name:     isAdjacent
+     *  Purpose:  Check if a vertex is adjacent to another vertex
+     *  Imports:
+     *    - vertex1 : A vertex used as source
+     *    - vertex2 : A vertex used as target
+     *  Exports:
+     *    - isAdjacent : boolean
+     **/
 
     public boolean isAdjacent(
         DSAGraphVertex<E,F> vertex1,
@@ -362,15 +539,42 @@ public class DSAGraph<E,F>
         return isAdjacent;
     }
 
+    /**
+     *  Name:     displayDepthFirstSearch
+     *  Purpose:  Displays the depth first search
+     *  Imports:
+     *    - none
+     *  Exports:
+     *    - none
+     **/
+
     public void displayDepthFirstSearch()
     {
         System.out.println(this.depthFirstSearch());
     }
 
+    /**
+     *  Name:     displayBreadthFirstSearch
+     *  Purpose:  Displays the breadth first search
+     *  Imports:
+     *    - none
+     *  Exports:
+     *    - none
+     **/
+
     public void displayBreadthFirstSearch()
     {
         System.out.println(this.breadthFirstSearch());
     }
+
+    /**
+     *  Name:     depthFirstSearch
+     *  Purpose:  Returns the depth first search
+     *  Imports:
+     *    - none
+     *  Exports:
+     *    - path : A String for the depth first search
+     **/
 
     public String depthFirstSearch()
     {
@@ -418,6 +622,15 @@ public class DSAGraph<E,F>
 
         return path;
     }
+
+    /**
+     *  Name:     breadthFirstSearch
+     *  Purpose:  Returns the breadth first search
+     *  Imports:
+     *    - none
+     *  Exports:
+     *    - path : A String for the breadth first search
+     **/
 
     public String breadthFirstSearch()
     {
@@ -468,6 +681,16 @@ public class DSAGraph<E,F>
         return path;
     }
 
+    /**
+     *  Name:     dijkstra
+     *  Purpose:  Get the shortest path from one vertex to another
+     *  Imports:
+     *    - label1 : A String for the label for the source vertex
+     *    - label2 : A String for the label for the target vertex
+     *  Exports:
+     *    - DSAStack which represents the path
+     **/
+
     public DSAStack<E> dijkstra(String label1, String label2)
     {
         DSAGraphVertex<E,F> vertex1 = null;
@@ -478,6 +701,16 @@ public class DSAGraph<E,F>
 
         return this.dijkstra(vertex1, vertex2);
     }
+
+    /**
+     *  Name:     dijkstra
+     *  Purpose:  Get the shortest path from one vertex to another
+     *  Imports:
+     *    - source : The source vertex
+     *    - terget : The target vertex
+     *  Exports:
+     *    - DSAStack which represents the path
+     **/
 
     public DSAStack<E> dijkstra(
         DSAGraphVertex<E,F> source,
@@ -499,6 +732,7 @@ public class DSAGraph<E,F>
         queue = new DSAMinHeap((int)Math.pow(vertexCount, 2));
         path = new DSAStack<E>();
 
+        // Set default values to all vertices and edges and add to the queue
         while (iter.hasNext())
         {
             tempVertex = iter.next();
@@ -516,31 +750,41 @@ public class DSAGraph<E,F>
             queue.add(tempVertex.getDistanceFromSource(), tempVertex);
         }
 
+        // While queue is not empty and haven't reached
         while (! queue.isEmpty() && ! reached)
         {
+            // Get vertex with the least distance from source
             tempVertex = (DSAGraph<E,F>.DSAGraphVertex<E,F>)queue.remove();
             iter = tempVertex.getAdjacent().iterator();
 
+            // For each adjacent vertex
             while (iter.hasNext() && ! reached)
             {
                 vertexAdjacent = iter.next();
 
+                // If it is the target, set to true
                 if (vertexAdjacent == target)
                 {
                     reached = true;
                 }
 
+                // If vertex has not yet been visited
                 if (! vertexAdjacent.getVisited())
                 {
                     vertexAdjacent.setVisited();
+
+                    // Get distance from source
                     alt = tempVertex.getDistanceFromSource()
                         + this.getEdgeWeight(
                             tempVertex,
                             vertexAdjacent
                     );
 
+                    // If distance is shorter than the recorded distance
+                    // from source
                     if (alt < vertexAdjacent.getDistanceFromSource())
                     {
+                        // Set distance and the vertex to access it
                         vertexAdjacent.setDistanceFromSource(alt);
                         vertexAdjacent.setPrevVertex(tempVertex);
                         queue.add(alt, vertexAdjacent);
@@ -549,6 +793,8 @@ public class DSAGraph<E,F>
             }
         }
 
+        // Starting at the target, push previous vertex
+        // to the stack until we've reached the source
         tempVertex = target;
 
         while (tempVertex != source)
@@ -557,10 +803,17 @@ public class DSAGraph<E,F>
             tempVertex = tempVertex.getPrevVertex();
         }
 
-        iter = vertices.iterator();
-
         return path;
     }
+
+    /**
+     *  Name:     markAllNew
+     *  Purpose:  Mark all verticies and edges as new
+     *  Imports:
+     *    - none
+     *  Exports:
+     *    - none
+     **/
 
     private void markAllNew()
     {
@@ -581,6 +834,15 @@ public class DSAGraph<E,F>
             }
         }
     }
+
+    /**
+     *  Name:     getSmallestKey
+     *  Purpose:  Get the smallest key in the graph
+     *  Imports:
+     *    - none
+     *  Exports:
+     *    - smallestKey : The label for the smallest key
+     **/
 
     private String getSmallestKey()
     {
@@ -606,6 +868,15 @@ public class DSAGraph<E,F>
         return smallestKey;
     }
 
+    /**
+     *  Name:     getNextNewAdjacent
+     *  Purpose:  Get the next adjacent vertex that is marked not visited
+     *  Imports:
+     *    - vertex : The vertex whose adjacent list will be checked
+     *  Exports:
+     *    - unvisitedVertex : The next unvisited adjacent vertex
+     **/
+
     private DSAGraphVertex<E,F> getNextNewAdjacent(
         DSAGraphVertex<E,F> vertex)
     {
@@ -624,6 +895,15 @@ public class DSAGraph<E,F>
 
         return unvisitedVertex;
     }
+
+    /**
+     *  Name:     validateLabel
+     *  Purpose:  Check if label is valid
+     *  Imports:
+     *    - inLabel : A string for the label
+     *  Exports:
+     *    - isUnique : Boolean
+     **/
 
     private boolean validateLabel(String inLabel)
     {
@@ -645,6 +925,15 @@ public class DSAGraph<E,F>
 
         return isUnique;
     }
+
+    /**
+     *  Name:     err
+     *  Purpose:  Formats an error line
+     *  Imports:
+     *    - msg : The error message
+     *  Exports:
+     *    - Formatted string
+     **/
 
     private String err(String msg)
     {
